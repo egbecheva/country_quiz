bdd6e59f514c89250e23c5947b176419511ea02e
Evelina <egbecheva@gmail.com>
2021-11-18 22:40:11 +0200

    Initial project setup and configurations

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader", //This is a webpack helper which allows to transpile Javascript files with babel and webpack. It uses babel under the hood
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader", //Install HTML web pack plugin and HTML loader for displaying our page
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html",
+    }),
+  ],
+}
