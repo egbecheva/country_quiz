b1419714020de61c3b21c1d8d06efbc293f6ea50
Evelina <egbecheva@gmail.com>
2021-11-20 23:50:41 +0200

    Fetch data from API and initiate dataMap

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
+  let results;
+  let obj = {};
+  let array = [];
+
+  const generateMap = (results) => {
+    results.map((item) => (obj[item?.capital?.[0] || item.name.common] = item.name.common));
+  };
+
+  const fetchAllData = () => {
+    fetch('https://restcountries.com/v3.1/all')
+      .then((response) => response.json())
+      .then((data) => (results = data))
+      .finally(() => generateMap(results));
+  };
+
+  useEffect(() => {
+    fetchAllData();
+  }, []);
+  console.log(obj);
   return (
     <h1>
-      <div>baba</div>
+      <div>test</div>
     </h1>
   );
 };
